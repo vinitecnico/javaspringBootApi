5e1fa2591980748bb5a359c98649858d434c50ee
vinicus rodrigues <vinitecnico@gmail.com>
2021-05-08 20:10:36 -0300

    update version

--- src/main/java/br/com/treinamento/springbootapi/config/SwaggerConfig.java
+++ src/main/java/br/com/treinamento/springbootapi/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package br.com.treinamento.springbootapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -9,11 +10,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
