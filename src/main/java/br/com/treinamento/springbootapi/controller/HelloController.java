4375dce005f490a3e876cdb97b35812ccbf24835
vinicus rodrigues <vinitecnico@gmail.com>
2021-05-07 22:54:48 -0300

    adjusted controller

--- src/main/java/br/com/treinamento/springbootapi/controller/HelloController.java
+++ src/main/java/br/com/treinamento/springbootapi/controller/HelloController.java
@@ -49,5 +49,31 @@ public class HelloController {
     {
         return _pessoaRepository.save(pessoa);
     }
+
+    @RequestMapping(value = "/pessoa/{id}", method =  RequestMethod.PUT)
+    public ResponseEntity<Pessoa> Put(@PathVariable(value = "id") long id, @RequestBody Pessoa newPessoa)
+    {
+        Optional<Pessoa> oldPessoa = _pessoaRepository.findById(id);
+        if(oldPessoa.isPresent()){
+            Pessoa pessoa = oldPessoa.get();
+            pessoa.setNome(newPessoa.getNome());
+            _pessoaRepository.save(pessoa);
+            return new ResponseEntity<Pessoa>(pessoa, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value = "/pessoa/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> Delete(@PathVariable(value = "id") long id)
+    {
+        Optional<Pessoa> pessoa = _pessoaRepository.findById(id);
+        if(pessoa.isPresent()){
+            _pessoaRepository.delete(pessoa.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
     
 }
